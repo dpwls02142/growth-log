451c211b7b2af4b47586e6734c8038d6393a53f2
dpwls <dpwls02142@gmail.com>
2025-06-01 20:26:26 +0900

    style: 오픈그래프, 파비콘 설정

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -48,8 +48,8 @@ const sidebar = (() => {
 export default defineConfig({
   base: '/',
   cleanUrls: true,
-  title: "Growth Log",
-  description: "개발자의 성장 일지",
+  title: "Growth Log : by dpwls",
+  description: "dpwls의 성장 일지",
   themeConfig: {
     outline: {
       level: [2, 3]
@@ -86,5 +86,21 @@ export default defineConfig({
   },
   head: [
     ['link', { rel: 'stylesheet', href: '/theme/custom.css' }],
+
+    // 파비콘 설정
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/public/favicon.ico' }],
+
+    // 오픈그래프 설정
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:title', content: 'Growth Log' }],
+    ['meta', { property: 'og:description', content: 'dpwls의 성장 일지' }],
+    ['meta', { property: 'og:image', content: '/public/og-image.jpg' }],
+    ['meta', { property: 'og:url', content: 'https://growth-log-kappa.vercel.app/' }],
+
+    // 트위터 카드 설정
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:title', content: 'Growth Log' }],
+    ['meta', { name: 'twitter:description', content: 'dpwls의 성장 일지' }],
+    ['meta', { name: 'twitter:image', content: '/public/og-image.jpg' }],
   ]
 }) 
